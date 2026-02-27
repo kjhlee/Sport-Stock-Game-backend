97a6798a796b896d9d8b15b5865e96f742a9b2a3
KJ Lee <kunnyjlee@gmail.com>
2026-02-27 11:49:18 -0800

    updated the register to take in a request, hash the password before adding it to the database

--- services/user-authentication/src/main/java/com/example/user_authentication/models/RegisterDetails.java
+++ services/user-authentication/src/main/java/com/example/user_authentication/models/RegisterDetails.java
@@ -2,15 +2,18 @@ package com.example.user_authentication.models;
 
 import java.util.UUID;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class RegisterDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- services/user-authentication/src/main/java/com/example/user_authentication/service/RegisterAccountService.java
+++ services/user-authentication/src/main/java/com/example/user_authentication/service/RegisterAccountService.java
@@ -4,16 +4,27 @@ import org.springframework.stereotype.Service;
 
 import com.example.user_authentication.models.RegisterDetails;
 import com.example.user_authentication.repository.RegisterAccountRepo;
+import com.example.user_authentication.security.SecurityConfig;
 
 @Service
 public class RegisterAccountService {
     private final RegisterAccountRepo accountRepo;
+    private final SecurityConfig passwordEncoder;
 
-    public RegisterAccountService(RegisterAccountRepo accountRepo) {
+    public RegisterAccountService(RegisterAccountRepo accountRepo, SecurityConfig passwordEncoder) {
         this.accountRepo = accountRepo;
+        this.passwordEncoder = passwordEncoder;
     }
     // add the account to the database 
-    public String registerAccount(RegisterDetails newAccount) throws Exception{
+    public String registerAccount(String email, String password) throws Exception{
+        RegisterDetails newAccount = new RegisterDetails();
+        newAccount.setEmail(email);
+
+        String hashedPassword = passwordEncoder.passwordEncoder().encode(password);
+        newAccount.setPassword(hashedPassword);
+        if(newAccount.getEmail() == null || newAccount.getPassword() == null) {
+            throw new Exception("Email or password cannot be null");
+        }
         if(accountRepo.existsByEmail(newAccount.getEmail())) {
             throw new Exception("Email already exists");
         }
