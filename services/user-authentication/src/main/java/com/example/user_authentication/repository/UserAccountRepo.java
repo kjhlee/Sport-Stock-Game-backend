095c8f65b258c4657db67f483630f7d8494f73a7
KJ Lee <kunnyjlee@gmail.com>
2026-02-28 15:46:15 -0800

    refactored the class names to make more sense

--- services/user-authentication/src/main/java/com/example/user_authentication/repository/UserAccountRepo.java
+++ services/user-authentication/src/main/java/com/example/user_authentication/repository/UserAccountRepo.java
@@ -5,9 +5,9 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.user_authentication.models.RegisterDetails;
+import com.example.user_authentication.models.UserDetails;
 
-public interface RegisterAccountRepo extends JpaRepository<RegisterDetails, UUID> {
+public interface UserAccountRepo extends JpaRepository<UserDetails, UUID> {
     boolean existsByEmail(String email);
-    Optional<RegisterDetails> findByEmail(String email);
+    Optional<UserDetails> findByEmail(String email);
 }
