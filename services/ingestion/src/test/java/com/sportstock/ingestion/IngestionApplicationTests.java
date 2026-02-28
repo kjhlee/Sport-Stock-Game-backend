14a448abebc9a900b0e3e5b200c2ce4e60ac49f6
Jonathan Kook <jkook95133@gmail.com>
2026-02-28 12:27:01 -0800

    setup

--- services/ingestion/src/test/java/com/sportstock/ingestion/IngestionApplicationTests.java
+++ services/ingestion/src/test/java/com/sportstock/ingestion/IngestionApplicationTests.java
@@ -1,13 +1,9 @@
 package com.sportstock.ingestion;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IngestionApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
