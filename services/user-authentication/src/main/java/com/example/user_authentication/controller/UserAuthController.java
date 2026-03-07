8e9b4091b6ac248eb06302ad444e7cd055c4f7c3
KJ Lee <kunnyjlee@gmail.com>
2026-03-07 11:20:23 -0800

    Finished the JWT workflow, users can now log in and validate their account

---
+++ services/user-authentication/src/main/java/com/example/user_authentication/controller/UserAuthController.java
@@ -0,0 +1,62 @@
+package com.example.user_authentication.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user_authentication.DTO.RefreshRequest;
+import com.example.user_authentication.DTO.RegisterRequest;
+import com.example.user_authentication.DTO.TokenResponse;
+import com.example.user_authentication.security.exceptions.TokenExpiredException;
+import com.example.user_authentication.service.LoginService;
+import com.example.user_authentication.service.RefreshService;
+import com.example.user_authentication.service.RegisterAccountService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserAuthController {
+    
+    private final RegisterAccountService accountService;
+    private final LoginService loginService;
+    private final RefreshService refreshService;
+    
+    @PostMapping("/register")
+    public ResponseEntity<String> registerAccount(@RequestBody RegisterRequest request) {
+        try {
+            accountService.registerAccount(request.getEmail(), request.getPassword());
+            return ResponseEntity.ok("Account registered successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody RegisterRequest request) {
+        try {
+            TokenResponse newToken = loginService.login(request.getEmail(), request.getPassword());
+            return ResponseEntity.ok(newToken);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());    
+        }
+    }
+    // this is a test mapping if the user has valid token they can access this endpoint 
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test successful");
+    }
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refresh(@RequestBody RefreshRequest request){
+        try {
+            String refreshToken = request.getRefreshToken();
+            String newAccessToken = refreshService.refreshAccessToken(refreshToken);
+            return ResponseEntity.ok(new TokenResponse(newAccessToken, refreshToken));
+        } catch (TokenExpiredException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
--- services/user-authentication/src/main/java/com/example/user_authentication/service/LoginService.java
+++ services/user-authentication/src/main/java/com/example/user_authentication/service/LoginService.java
@@ -1,28 +1,37 @@
 package com.example.user_authentication.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.user_authentication.DTO.TokenResponse;
 import com.example.user_authentication.models.UserDetails;
 import com.example.user_authentication.repository.UserAccountRepo;
-import com.example.user_authentication.security.SecurityConfig;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
+    private final BCryptPasswordEncoder passwordEncoder;
+    
+    private final JwtService jwtService;
+
     private final UserAccountRepo accountRepo;
-    private final SecurityConfig sc;
 
+    public TokenResponse login(String email, String password) {
+        UserDetails account = accountRepo.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
 
-    public void login(String email, String password) {
-        UserDetails account = accountRepo.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
-        if (sc.passwordEncoder().matches(password, account.getPassword())) {
-            // Authentication successful
-            System.out.println("Login successful!");
-        } else {
+        if (!passwordEncoder.matches(password, account.getPassword())) {
             throw new RuntimeException("Invalid credentials");
         }
+
+        String accessToken = jwtService.generateAccessToken(email);
+        String refreshToken = jwtService.generateRefreshToken(email);
+
+        return new TokenResponse(accessToken, refreshToken);
     }
 }
