4101abfdb00481063e5ba9625ee509619e1ba041
ethankook <93116378+ethankook@users.noreply.github.com>
2026-03-25 00:32:05 -0700

    spotless

--- services/stock-market/src/main/java/com/sportstock/stockmarket/config/SecurityConfig.java
+++ services/stock-market/src/main/java/com/sportstock/stockmarket/config/SecurityConfig.java
@@ -18,10 +18,7 @@ public class SecurityConfig {
         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
             auth ->
-                auth.requestMatchers("/api/v1/stocks/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated())
+                auth.requestMatchers("/api/v1/stocks/**").permitAll().anyRequest().authenticated())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
