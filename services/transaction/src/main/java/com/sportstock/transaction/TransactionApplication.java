787b6c5c75d302f40e6fe4507c8fc5b0cf397065
ethankook <93116378+ethankook@users.noreply.github.com>
2026-03-24 23:24:48 -0700

    feat: enable scheduling in transaction service, improve stipend scheduling logic, and utilize dynamic variables in ingestion/league flows

--- services/transaction/src/main/java/com/sportstock/transaction/TransactionApplication.java
+++ services/transaction/src/main/java/com/sportstock/transaction/TransactionApplication.java
@@ -2,8 +2,10 @@ package com.sportstock.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.sportstock"})
+@EnableScheduling
 public class TransactionApplication {
 
   public static void main(String[] args) {
