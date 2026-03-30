8aad8b05f1b51d20e7897200deee933c124d9ee8
ethankook <93116378+ethankook@users.noreply.github.com>
2026-03-30 13:13:12 -0700

    spotless

--- services/common/src/main/java/com/sportstock/common/security/BaseSecurityConfig.java
+++ services/common/src/main/java/com/sportstock/common/security/BaseSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -15,11 +16,13 @@ public class BaseSecurityConfig {
   @Bean
   @ConditionalOnMissingBean(SecurityFilterChain.class)
   public SecurityFilterChain securityFilterChain(
-      HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter,
-      CorsConfigurationSource corsConfigurationSource) throws Exception {
+      HttpSecurity http,
+      JwtAuthenticationFilter jwtAuthenticationFilter,
+      CorsConfigurationSource corsConfigurationSource)
+      throws Exception {
 
     http.cors(cors -> cors.configurationSource(corsConfigurationSource))
-        .csrf(csrf -> csrf.disable())
+        .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
